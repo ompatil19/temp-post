d42ae048203c4aec9879da20ef22ed36db8b8fea
ompatil19 <ompatil2002@gmail.com>
2024-01-14 02:39:37 +0530

    changed app.js

--- app.js
+++ app.js
@@ -11,13 +11,26 @@ app.post('/api/data', (req, res) => {
         }
         console.log('POST request successful');
         console.log('Data:', postData); 
-        res.status(201).json({ message: 'POST request successful', data: postData });
+
+        res.status(201).json({ message: 'POST request successful', data: [
+            {
+              "name": "Gokarna",
+              "imagePath": "ImageConstant.imgImage3",
+              "description": "Om Beach, Gokarna, Karnataka",
+              "seasonText": ["Oct-Jan", "Feb-Jun", "Jul-Sept"]
+            },
+            {
+              "name": "Pondicherry",
+              "imagePath": "ImageConstant.imgImage4",
+              "description": "Rock Beach, Pondicherry, Tamilnadu",
+              "seasonText": ["May-Aug", "Sept-Dec", "Jan-Apr"]
+            }
+          ]},);
     } catch (error) {
         console.error('Error processing POST request:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-
 app.listen(3001, () => {
     console.log('Server is running on port 3000');
 });
